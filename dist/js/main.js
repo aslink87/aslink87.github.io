3496f75a4ef85c741c158ed9481efca7e7b12550
adam.slinkman@gmail.com <adam.slinkman@gmail.com>
2020-07-01 14:13:23 -0400

    Initial Release

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,19 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+//Tab Hover Effects
+var iconBx = document.querySelectorAll(".iconBx");
+var contentBx = document.querySelectorAll(".contentBx");
+
+for (var i = 0; i < iconBx.length; i++)
+  iconBx[i].addEventListener("mouseover", function () {
+    for (var i = 0; i < contentBx.length; i++) {
+      contentBx[i].className = "contentBx";
+    }
+    document.getElementById(this.dataset.id).className = "contentBx active";
+    for (var i = 0; i < iconBx.length; i++) {
+      iconBx[i].className = "iconBx";
+    }
+    this.className = "iconBx active";
+  });
